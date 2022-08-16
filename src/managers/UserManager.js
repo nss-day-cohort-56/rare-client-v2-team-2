38ce1066ca6044f0c038bf6436eb78295deac8b5
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-08-16 10:05:31 -0500

    filtering by category, user, title

---
+++ src/managers/UserManager.js
@@ -0,0 +1,17 @@
+export const getAllUsers = () => {
+    return fetch("http://localhost:8000/users", {
+        headers: {
+            'Authorization': `Token ${localStorage.getItem('auth_token')}`
+          }
+    })
+    .then(res => res.json())
+}
+
+export const getUserById = id => {
+    return fetch(`http://localhost:8000/users/${id}`, {
+        headers: {
+            'Authorization': `Token ${localStorage.getItem('auth_token')}`
+          }
+    })
+    .then(res => res.json())
+};
