2f71170aabb492f272bd226a874762368cb61945
Wesley Harllee <wesharllee@gmail.com>
2022-08-16 15:40:02 -0500

    updated user edit

--- src/managers/UserManager.js
+++ src/managers/UserManager.js
@@ -12,6 +12,17 @@ export const getUserById = (id) => {
             'Authorization': `Token ${localStorage.getItem('auth_token')}`
         }
     })
-    .then(res => res.json())
+        .then(res => res.json())
+};
+
+export const updateUser = (id, user) => {
+    return fetch(`http://localhost:8000/users/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Token ${localStorage.getItem('auth_token')}`
+        },
+        body: JSON.stringify(user)
+    }).then((res) => localStorage.setItem("is_staff", res.is_staff))
 }
 
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -13,6 +13,7 @@ import { CommentForm } from "../components/comments/CommentForm"
 import { CommentsList } from "../components/comments/CommentList"
 import { Users } from "../components/users/UserList"
 import { UserDetail } from "../components/users/UserDetail"
+import { UserEdit } from "../components/users/UserEdit"
 
 
 export const ApplicationViews = ({ isStaff, token, setToken, setUserId, userId }) => {
@@ -37,6 +38,7 @@ export const ApplicationViews = ({ isStaff, token, setToken, setUserId, userId }
             ? <Route path="/users">
               <Route index element={<Users />} />
               <Route path=":userId" element={<UserDetail />} />
+              <Route path=":userId/edit" element={<UserEdit />} />
             </Route>
             : <Route path="/users" element={<Navigate to="/posts" replace />} />
         }
