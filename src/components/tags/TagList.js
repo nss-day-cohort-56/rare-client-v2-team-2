175374d7087afa86b79862c4de17c6c4a1c2da1b
Vicki Taylor <vtaylor85@live.com>
2022-08-16 10:03:03 -0500

    getting main

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { getAllTags, deleteTag } from "../../managers/TagManager"
 import { TagForm } from "./TagForm"
 
-export const TagList = () => {
+export const TagList = ({ isStaff }) => {
   const [tags, setTags] = useState([])
   const [editTag, setEditTag] = useState({ label: '' })
 
@@ -21,31 +21,40 @@ export const TagList = () => {
 
   return <section className="section">
     <div className="columns">
-      <div className="column">
-        <table className="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Tags</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              tags.map(tag => (
-                <tr key={tag.id}>
-                  <td>{tag.label}</td>
-                  <td>
-                    <div className="buttons">
-                      <button className="button is-warning" onClick={() => { setEditTag(tag) }}>edit</button>
-                      <button className="button is-danger" onClick={() => { handleDelete(tag.id) }}>delete</button>
-                    </div>
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </div>
+
+      {isStaff
+        ? <div className="column">
+          <table className="table is-fullwidth">
+            <thead>
+              <tr>
+                <th>Tags</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                tags.map(tag => (
+                  <tr key={tag.id}>
+                    <td>{tag.label}</td>
+                    <td>
+                      <div className="buttons">
+                        <button className="button is-warning" onClick={() => { setEditTag(tag) }}>edit</button>
+                        <button className="button is-danger" onClick={() => {
+                          const confirmBox = window.confirm("Do you really want to delete this tag?")
+                          if (confirmBox)
+                            handleDelete(tag.id)
+                        }}>delete</button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+        : ""
+}
+
       <div className="column">
         <TagForm loadTags={loadTags} tag={editTag} setTag={setEditTag} />
       </div>
