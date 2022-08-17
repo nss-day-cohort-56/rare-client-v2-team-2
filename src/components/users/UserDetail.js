7644468e1ce3fcd3e072bd76ac13f7d9cf25eaf5
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-08-17 11:26:13 -0500

    user able to change profile picture

--- src/components/users/UserDetail.js
+++ src/components/users/UserDetail.js
@@ -1,4 +1,4 @@
-import { getUserById } from "../../managers/UserManager";
+import { getUserById, updateUser } from "../../managers/UserManager";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -6,6 +6,8 @@ export const UserDetail = () => {
     const [rareUser, setRareUser] = useState([])
     const { userId } = useParams()
     const navigate = useNavigate()
+    const [imageString, setString] = useState("")
+    const [profileImage, setImage] = useState("")
 
     useEffect(() => {
         getUserById(userId)
@@ -30,6 +32,14 @@ export const UserDetail = () => {
         }
     }
 
+    useEffect(
+        () => {
+            let image = rareUser?.profile_image_url
+            setImage(image)
+        },
+        [rareUser]
+    )
+
     // useEffect(() => {console.log(rareUser)}, [])
 
     let userName = rareUser?.user?.username
@@ -41,14 +51,20 @@ export const UserDetail = () => {
     let bio = rareUser?.bio
     let profileType = staff(rareUser?.user)
     let isActive = active(rareUser?.user)
-
-    const handleImageChange = (e) => {
-        let newProfileData = {
-          ...rareUser,
-          profile_image_url: e.target.files[0]
-        }
+    
+    const getBase64 = (file, callback) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(file);
     }
     
+    const createProfilePictureString = (event) => {
+        getBase64(event.target.files[0], (base64ImageString) => {
+            setString(base64ImageString)
+    
+            // Update a component state variable to the value of base64ImageString
+        });
+    }
 
     return( 
         <>
@@ -56,12 +72,19 @@ export const UserDetail = () => {
                 <div className="userTitle">Users</div>
                 <section className="userBox" key={rareUser.id}>
                     <div className="user" >
-                        <img src={profileImg} alt="userImage" className="userImage" />
-                        <p>
-                        <input type="file"
-                            id="image"
-                            accept="image/png, image/jpeg"  onChange={(event) => {handleImageChange(event)}} required/>
-                        </p>
+                        <img src={`http://localhost:8000${profileImage}`} alt="userImage" className="userImage" />
+                        <input type="file" id="image" onChange={createProfilePictureString} />
+                        <input type="hidden" name="user_id" value={userId} />
+                        <button onClick={() => {
+                            let newUserData = {
+                                ...rareUser,
+                                image: imageString
+                            }
+                            updateUser(userId, newUserData).then(() => {
+                                getUserById(userId)
+                                .then(setRareUser)
+                            })
+                        }}>Upload</button>
                         <div value={rareUser.id}>Bio: {bio}</div>
                         <div value={rareUser.id}>Name: {firstName} {lastName}</div>
                         <div value={rareUser.id}>User Name: {userName}</div>
